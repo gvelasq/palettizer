385c3ec3e4aa295a03e00f22d189380f68a3b699
Gustavo Velásquez <gvelasq2github@gmail.com>
2022-03-11 12:25:09 -0800

    Add `palettize()`

---
+++ R/palettize.R
@@ -0,0 +1,18 @@
+#' Create a color palette
+#'
+#' @description
+#' `palettize()` creates a color palette from an image file.
+#'
+#' @usage
+#' palettize(path)
+#'
+#' @param path A path to a supported image file.
+#'
+#' @return
+#' A character vector of hexadecimal colors.
+#'
+#' @rdname palettize
+#' @export
+palettize <- function(path) {
+
+}
