1cf647cab130497f4ba2ecc053aa6fb7ccff2da7
Gustavo Velásquez <gvelasq2github@gmail.com>
2022-03-24 12:49:41 -0700

    Fix sort_type argument

--- R/plt_tize.R
+++ R/plt_tize.R
@@ -4,12 +4,12 @@
 #' `plt_tize()` creates a color palette from a supported image file.
 #'
 #' @usage
-#' plt_tize(path, cluster_count, seed, sort_type)
+#' plt_tize(path, cluster_count, seed, sort_type = "weight")
 #'
 #' @param path A path to a supported image file.
 #' @param cluster_count The number of clusters for k-means clustering.
 #' @param seed An integer to specify the seed for the random number generator.
-#' @sort_type A character vector, one of "weight" (the default), "red", "green",
+#' @param sort_type A character vector, one of "weight" (the default), "red", "green",
 #' or "blue".
 #'
 #' @return
@@ -17,6 +17,6 @@
 #'
 #' @rdname plt_tize
 #' @export
-plt_tize <- function(source_path, cluster_count, seed, sort_type) {
+plt_tize <- function(path, cluster_count, seed, sort_type = "weight") {
   NULL
 }
