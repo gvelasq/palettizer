3bf392364f9a9f760cc1002a28b5ee45fe116dc5
Gustavo Velásquez <gvelasq2github@gmail.com>
2022-03-24 10:56:58 -0700

    Add arguments to `plt_tize()`

--- R/plt_tize.R
+++ R/plt_tize.R
@@ -4,9 +4,13 @@
 #' `plt_tize()` creates a color palette from a supported image file.
 #'
 #' @usage
-#' plt_tize(path)
+#' plt_tize(path, cluster_count, seed, sort_type)
 #'
 #' @param path A path to a supported image file.
+#' @param cluster_count The number of clusters for k-means clustering.
+#' @param seed An integer to specify the seed for the random number generator.
+#' @sort_type A character vector, one of "weight" (the default), "red", "green",
+#' or "blue".
 #'
 #' @return
 #' A character vector of hexadecimal colors.
